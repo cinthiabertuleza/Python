2b029adc66bd1cd79578f181a12c855cd631d9eb
cinthiabertuleza <101706603+cinthiabertuleza@users.noreply.github.com>
2022-04-25 18:47:23 -0300

    Update and rename Desafo023.py to VerificaCidade.py

--- Desafo023.py
+++
@@ -1,3 +0,0 @@
-cid = str(input('Em que cidade você nasceu?')).strip()
-print(cid[:6].upper() == 'AFONSO')
-#Verifica se você realmente nasceu numa cidade que começa com Afonso
---
+++ VerificaCidade.py
@@ -0,0 +1,3 @@
+cid = str(input('Em que cidade você nasceu?')).strip()
+print(cid[:6].upper() == 'AFONSO BEZERRA')
+#Verifica se você realmente nasceu numa cidade que começa com Afonso
