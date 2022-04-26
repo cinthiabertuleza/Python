28d915420b4955c5c6bebf7f74cec4bd64dae4dd
Cinthia Bertuleza <101706603+cinthiabertuleza@users.noreply.github.com>
2022-04-26 12:55:09 -0300

    Delete MenoreMaior.py

--- MenoreMaior.py
+++
@@ -1,8 +0,0 @@
-n1 = int(input('Digite o primeiro número: '))
-n2 = int(input('Digite o segundo número: '))
-if n1 > n2:
-    print('O primeiro valor é maior.')
-elif n2 > n1:
-    print('O segundo valor é o maior.')
-else:
-    print('Não existe valor maior, os dois são iguais.')
